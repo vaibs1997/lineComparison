8c59e0f305289dfe42c79129e741edb532e2cc71
vaibs <vaibhavmuratkar2@gmail.com>
2021-09-10 00:27:53 +0530

    length of line

---
+++ src/Blabz/lineComparison/lineComparison.java
@@ -0,0 +1,17 @@
+package Blabz.lineComparison;
+
+public class lineComparison
+{
+    public static void main(String args[]){
+        int x1=10;
+        int x2=20;
+        int y1=12;
+        int y2=27;
+        int result;
+        result= (int) (Math.sqrt((x2-x1)^2+(y2-y1)^2));
+
+        System.out.println("length of line"+result);
+
+    }
+
+}
